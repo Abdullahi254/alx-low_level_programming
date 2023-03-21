a896b107568ae34163215980ebcfaf6154b51dc9
Abdullahi <abdush504@gmail.com>
2023-03-21 04:37:20 -0700

    done second task(sometimes suffer from insomnia.)

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
