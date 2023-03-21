69754d5601016cfedf99337424d6db3601af2311
Abdullahi <abdush504@gmail.com>
2023-03-21 04:06:01 -0700

    fixed multiple defination of putchar in 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include <string.h>
 #include "main.h"
 /**
  * main - Entry point
@@ -9,9 +8,8 @@
 int main(void)
 {
 	int i;
-	char myString[15];
+	char myString[15] = "_putchar";
 
-	strcpy(myString, "_putchar");
 	for (i = 0; i < 8; i++)
 	{
 		putchar(myString[i]);
