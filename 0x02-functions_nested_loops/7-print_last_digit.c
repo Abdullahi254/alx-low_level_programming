21ce8500fb47cb42d09ef8bb1295352771ca820d
Abdullahi <abdush504@gmail.com>
2023-03-21 10:13:42 -0700

    made sure input is an absolute int before proceeding

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,23 @@
 #include "main.h"
 /**
  * print_last_digit - prints last digit of a number
- * @i: input variable
+ * @i: input variabe
  * Return: last digit of input
  */
 int print_last_digit(int i)
 {
 	int lastDigit;
 
-	lastDigit = i % 10;
-	return (lastDigit);
+	if (i < 0)
+	{
+		lastDigit = -1 * (i % 10);
+		_putchar(lastDigit + '0');
+		return (lastDigit);
+	}
+	else
+	{
+		lastDigit = i % 10;
+		_putchar(lastDigit + '0');
+		return (lastDigit);
+	}
 }
