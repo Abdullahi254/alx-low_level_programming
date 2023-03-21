98c9452c11450d41a290d9ddc9f1eefa17cdfb02
Abdullahi <abdush504@gmail.com>
2023-03-21 03:41:23 -0700

    added a Readme file and done the first task(_putchar)

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char myString[15];
+
+	strcpy(myString, "_putchar");
+	for (i = 0; i < 8; i++)
+	{
+		putchar(myString[i]);
+	}
+	putchar('\n');
+	return (0);
+}
