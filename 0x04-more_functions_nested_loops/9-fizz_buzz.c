6af0ef69724098303ee3037221c79871036748a5
Abdullahi <abdush504@gmail.com>
2023-03-23 12:57:32 -0700

    a program that print 1-100 with a twist

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - code entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+			{
+				putchar('F');
+				putchar('i');
+				putchar('z');
+				putchar('z');
+			}
+			if (i % 5 == 0)
+			{
+				putchar('B');
+				putchar('u');
+				putchar('z');
+				putchar('z');
+			}
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
