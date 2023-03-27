a69df1f58f0e12443f37ae82e70e59d98985f446
Abdullahi <abdush504@gmail.com>
2023-03-27 13:03:51 -0700

    fixed error where output printed additional 1 byte

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,26 @@
 #include "main.h"
+
 /**
- * print_rev - prints a string in reverse
- * @s: pointer param to string
- * Return: void
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
  */
 void print_rev(char *s)
 {
-	int i;
+	int i, j, len;
 
 	i = 0;
-	while ((*(s + i)) != '\0')
+
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-	while (i >= 0)
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
 	{
-		_putchar(*(s + i));
-		i--;
+		_putchar(s[j]);
 	}
+
 	_putchar('\n');
 }
