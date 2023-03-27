638ef23866035c96b6fd0ca558083d53a84dba29
Abdullahi <abdush504@gmail.com>
2023-03-27 04:12:48 -0700

    added a pointer array directory and created first function that takes pointer and updates the value it points to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - takes a pointer as param and updates value it points
+ * @n: param pointer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
