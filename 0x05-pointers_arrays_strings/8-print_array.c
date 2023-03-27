4877a9b3b2db94b41bb397771403a4808225f57f
Abdullahi <abdush504@gmail.com>
2023-03-27 12:54:44 -0700

    made sure it adheres to betty

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,14 +5,14 @@
  * @n: number of elements of the array to be printed
  * Return: void
  */
-void print_array (int *a, int n)
+void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
 		if (i == (n - 1))
-		{	
+		{
 			printf("%d", a[i]);
 		}
 		else
