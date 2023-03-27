f58faf498c05b10d2454743a93f44d2afa29973d
Abdullahi <abdush504@gmail.com>
2023-03-27 07:50:29 -0700

    fixed bug that ocassionally missed to print last character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,18 @@
  */
 void puts2(char *str)
 {
-	int i;
-
+	int i, j;
+	
 	i = 0;
-	while (str[i] != '\0')
+	j = 0;
+	while (*(str + i) != '\0')
 	{
-		_putchar(str[i]);
-		i = i + 2;
+		i++;
+	}
+
+	for (j = 0; j < i; j = j + 2)
+	{
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
