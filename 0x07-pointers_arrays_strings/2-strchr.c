6d59788d31381aa5515e4fa0e41c0758ace7c255
Abdullahi <abdush504@gmail.com>
2023-04-03 05:02:15 -0700

    added a strchr clone(char locator in string)

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a char in a string
+ * @s: pointer param to the string that is to be traversed
+ * @c: character to be looked for
+ * Return: pointer to the first occurence of the character c or null
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
