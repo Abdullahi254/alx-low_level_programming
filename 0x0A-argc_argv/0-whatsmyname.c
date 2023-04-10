b27f47ef1f496c20962151c7fbef2b100e4935af
Abdullahi <abdush504@gmail.com>
2023-04-10 11:23:41 -0700

    added a program that prints the program's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ * @argc: conter param
+ * @argv: argument vector
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
