03ebac9cd8760159a07f96faa1c862c88ab9d712
Abdullahi <abdush504@gmail.com>
2023-04-10 12:02:53 -0700

    added a program that prints all arguments it received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - prints file name
+ *@argc: argument count
+ *@argv: argument vector
+ *DESCRIPTION: prints file name
+ *Return: Always (0) Success
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", *argv);
+		argv++;
+		i++;
+	}
+	return (0);
+}
