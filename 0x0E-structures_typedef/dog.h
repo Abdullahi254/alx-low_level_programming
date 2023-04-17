d4ad52f9e4055944a0be3fda918510ecc257794b
Abdullahi <abdush504@gmail.com>
2023-04-17 12:11:38 -0700

    Defined a new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,14 @@
  *@age: integer
  *@owner: character string
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-};
+} dog_t;
 #endif
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
