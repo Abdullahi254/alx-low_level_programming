7e85fcbdca755d5664e297d15ab2dbfdc2c09502
Abdullahi <abdush504@gmail.com>
2023-04-20 14:26:28 -0700

    betty fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,11 +10,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
+	char *str;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		char *str;
 		str = va_arg(args, char*);
 		if (str == NULL)
 			printf("(nil)");
