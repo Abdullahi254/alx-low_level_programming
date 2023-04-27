2fb515116056fa2e690126c1ebca6129856f2614
Abdullahi <abdush504@gmail.com>
2023-04-27 13:17:21 -0700

    fixed bug

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,11 +15,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	newNode = malloc(sizeof(list_t));
 	newNode->str = strdup(str);
-	if (newNode->str == NULL)
-	{
-		free(newNode);
-		return (NULL);
-	}
 	newNode->len = _strlen(str);
 	newNode->next = *head;
 	*head = newNode;
