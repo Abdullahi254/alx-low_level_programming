127e83887a242ffd78b50f0f9bfd8f73a1594cca
Abdullahi <abdush504@gmail.com>
2023-04-27 13:44:30 -0700

    added custom _strlen

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	new_node = malloc(sizeof(list_t));
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = NULL;
 	if (*head == NULL)
 		*head = new_node;
@@ -30,4 +30,20 @@ list_t *add_node_end(list_t **head, const char *str)
 		current->next = new_node;
 	}
 	return (new_node);
+}
+/**
+ *_strlen - returns the length ofa string
+ *@s: pointer to a string
+ *Return: string length
+ */
+int _strlen(const char *s)
+{
+	int a;
+	int len;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		len++;
+	}
+	return (a);
 }
