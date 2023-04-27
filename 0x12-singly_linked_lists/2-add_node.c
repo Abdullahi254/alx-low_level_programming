b26af5246b57a790819453277b510e3c83c31bb2
Abdullahi <abdush504@gmail.com>
2023-04-27 13:41:58 -0700

    changed the naming convention

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,14 +9,14 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newNode;
+	list_t *new_node;
 
 	if (str == NULL)
 		return (NULL);
-	newNode = malloc(sizeof(list_t));
-	newNode->str = strdup(str);
-	newNode->len = strlen(str);
-	newNode->next = *head;
-	*head = newNode;
-	return (newNode);
+	new_node = malloc(sizeof(list_t));
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
