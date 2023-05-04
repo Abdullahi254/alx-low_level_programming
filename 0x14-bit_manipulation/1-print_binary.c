db9a4e50f547ea43954805c84d21acd578557639
Abdullahi <abdush504@gmail.com>
2023-05-04 13:01:22 -0700

    added a function that prints binary rep of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - prints binary
+ * @n: int to be converted to binary
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int i;
+	unsigned long int mask;
+
+	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	for (i = 0; i < sizeof(unsigned long int) * 8; i++)
+	{
+		if (n & mask)
+		{
+			printf("1");
+		}
+		else
+			printf("0");
+		mask >>= 1;
+	}
+}
