fee5a32ce6fbf5ce00a7359e71a5c4e1831b715f
Abdullahi <abdush504@gmail.com>
2023-06-15 04:39:12 -0700

    added my custom preprocessor file

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct my_double_linked - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ */
+typedef struct my_double_linked
+{
+	int n;
+	struct my_double_linked *prev;
+	struct my_double_linled *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+#endif
