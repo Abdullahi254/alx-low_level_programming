01f0a6601af93d335dadc37591a7dd018156e3f7
Abdullahi <abdush504@gmail.com>
2023-06-15 12:26:09 -0700

    added a function that deletes a node at an index

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,18 +6,21 @@
  *@n: integer to be copied
  *Return: NULL if fail or a null element else new node
  */
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
+	if (!new_node)
+	{
 		return (NULL);
+	}
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
+	if (*head)
+	{
+		(*head)->prev = new_node;
+	}
 	*head = new_node;
 	return (new_node);
 }
