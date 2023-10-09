79a860a9e26118fd98b009d409f48d88e4b4822c
Abdullahi <abdush504@gmail.com>
2023-10-09 04:53:19 -0700

    linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+/**
+ * linear_search - performs linear search
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of value
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (*array == value)
+		{
+			return (i);
+		}
+		array += 1;
+	}
+	return (-1);
+}
