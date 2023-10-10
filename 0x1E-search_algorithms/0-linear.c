96e75aa9b3acc9fc35d7f3f202659a40ecaa6b38
Abdullahi <abdush504@gmail.com>
2023-10-10 03:55:24 -0700

    arra[i] instrad of *array whan accessing data from a apointer

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,11 +18,10 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%li] = [%i]\n", i, array[i]);
-		if (*array == value)
+		if (array[i] == value)
 		{
 			return (i);
 		}
-		array += 1;
 	}
 	return (-1);
 }
